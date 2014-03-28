91ed8b3cfb8ce0a913dbfec1e52a9aebb6da3674
Tony Han <h.bing612@gmail.com>
2014-03-29 00:42:15 +0800

    implement class __super__

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ module.exports = function(definition, parentClass) {
   var klass = definition.initialize || function(){};
   if (parentClass) {
     klass.prototype = new parentClass();
+    klass.__super__ = parentClass;
+  } else {
+    klass.__super__ = Object;
   }
   delete definition.initialize;
   for (var prop in definition) {
