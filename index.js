00c0c95016213c951a349688432d3a6d91cccde8
Tony Han <h.bing612@gmail.com>
2014-03-29 01:10:21 +0800

    implement super method of klass's prototype

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 module.exports = function(definition, parentClass) {
   var klass = definition.initialize || function(){};
+
   if (parentClass) {
     klass.prototype = new parentClass();
     klass.__super__ = parentClass;
   } else {
     klass.__super__ = Object;
   }
+
   delete definition.initialize;
   for (var prop in definition) {
     if (definition.hasOwnProperty(prop)) {
       klass.prototype[prop] = definition[prop];
     }
   }
+  klass.prototype.super = function(methodName) {
+    var args = Array.prototype.slice.call(arguments, 1); // [] === Array.prototype
+    var superClass = klass.__super__;
+    if (superClass.prototype.hasOwnProperty(methodName)) {
+      return superClass.prototype[methodName].apply(this, args);
+    }
+  }
+
   klass.prototype.constructor = klass;
   return klass;
 };
