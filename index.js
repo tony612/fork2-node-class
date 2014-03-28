3b08fbc70087871b29ea4fbc41f22e15e3d8610f
Tony Han <h.bing612@gmail.com>
2014-03-29 00:37:19 +0800

    implement the basic inheritance

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-module.exports = function(definition) {
+module.exports = function(definition, parentClass) {
   var klass = definition.initialize || function(){};
+  if (parentClass) {
+    klass.prototype = new parentClass();
+  }
   delete definition.initialize;
-  klass.prototype = definition;
+  for (var prop in definition) {
+    if (definition.hasOwnProperty(prop)) {
+      klass.prototype[prop] = definition[prop];
+    }
+  }
   klass.prototype.constructor = klass;
   return klass;
 };
