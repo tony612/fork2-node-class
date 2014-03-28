c79e8786a11f415710b1a183e875668ce78d1bae
Tony Han <h.bing612@gmail.com>
2014-03-28 16:39:11 +0800

    fix class constructor test

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = function(definition) {
   var klass = definition.initialize || function(){};
   delete definition.initialize;
   klass.prototype = definition;
+  klass.prototype.constructor = klass;
   return klass;
 };
