9111cb937c640c6d39d5d71d6b5ed096715d5fd0
Tony Han <h.bing612@gmail.com>
2014-03-29 02:43:31 +0800

    add comment for super function

--- index.js
+++ index.js
@@ -14,7 +14,25 @@ module.exports = function(definition, parentClass) {
       klass.prototype[prop] = definition[prop];
     }
   }
+
   var currentClass = klass;
+  // Everytime this.super calls this class's super method
+  // But currentClass is different
+  // e.g.
+  // var A = Class({foo: function() { return 1; }});
+  // var B = Class({foo: function() { this.super('foo') }});
+  // var C = Class({foo: function() { this.super('bar') }});
+  // var c = new C();
+  // c.foo();
+  // -> C#foo
+  //      this[c].super('bar');
+  //      -> This function: currentClass = C change=> B
+  //      -> B.prototype.foo.apply(this, args);
+  //        -> B#foo
+  //          this[c].super('bar')
+  //          -> This function: currentClass = B change=> A
+  //          -> A.prototype.foo.apply(this, args);
+  //
   klass.prototype.super = function(methodName) {
     var args = Array.prototype.slice.call(arguments, 1); // [] === Array.prototype
     currentClass = currentClass.__super__;
